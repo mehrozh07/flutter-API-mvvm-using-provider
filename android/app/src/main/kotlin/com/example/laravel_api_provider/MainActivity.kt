45206313d69a9d896cc07399f8a18dea7a3b89c0
Mehroz <mehrozhassan99@gmail.com>
2023-02-09 00:14:08 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/laravel_api_provider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.laravel_api_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Data/exceptions.dart
@@ -0,0 +1,36 @@
+class AppException implements Exception{
+  final _message;
+  final _prefix;
+
+  AppException([this._message, this._prefix]);
+
+  @override
+  String toString(){
+    return "$_message$_prefix";
+  }
+
+}
+
+class NetworkExceptions extends AppException{
+  NetworkExceptions([String? message]) : super(message, "Error during communications");
+}
+
+class AuthorizationExceptions extends AppException{
+  AuthorizationExceptions([String? message]) : super(message, "Error in Authorizations");
+}
+
+class RateLimitExceptions extends AppException{
+  RateLimitExceptions([String? message]) : super(message, "Request Rate Limit Exceeded");
+}
+
+class BadRequestExceptions extends AppException{
+  BadRequestExceptions([String? message]) : super(message, "Bad requests occurs or missing parameters");
+}
+
+class ServerSideExceptions extends AppException{
+  ServerSideExceptions([String? message]) : super(message, "Server Errors occurring");
+}
+
+class UnHandledExceptions extends AppException{
+  UnHandledExceptions([String? message]) : super(message, "UnExpected behaviour");
+}
